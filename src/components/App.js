be6cead5790597d293e4312fc7656786abc27f5e
danish <danishjamadar9089@gmail.com>
2023-07-13 18:19:23 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <div>
         {/* Do not remove the main div */}
         <p>Button clicked {count} times</p>
-        <button onClick={setCount=()=>(count+1)}>Click Me</button>
+        <button onClick={()=>setCount(count+1)}>Click Me</button>
         
     </div>
   )
